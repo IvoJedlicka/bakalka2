fb0d13c4a36929427eadfb4ee88bd5406b69877f
IvoJedlicka <jedlicka@bakalka.cz>
2024-05-16 07:37:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 // Matice obsahující textové hodnoty
 let maticeText = [
-    ["Text 1", "Text 2", "Text 3"],
-    ["Text 4", "Text 5", "Text 6"],
-    ["Text 7", "Text 8", "Text 9"]
+    ["-", "A", "B", "C", "D", "E"],
+    ["MOTIVACE A AKTIVITA PŘI VÝUCE", 
+     "Žák je aktivní, jeví upřímný zájem o předmět a touhu postupovat kupředu. Během vyučování se vždy aktivně zapojuje.", 
+     "Žák je zpravidla aktivní, pokud se mu daří. Během vyučování se aktivně zapojuje.",
+     "Žák má výkyvy v přístupu, jeho aktivita v hodině se objevuje nepravidelně.",
+    "Žák je aktivní jen výjimečně, sám nevyvíjí žádnou aktivitu, ale reaguje pozitivně na domluvu."
+     "Žák je v přístupu zcela pasivní. Nereaguje na domluvu. Negativně působí na třídu, vyrušuje a strhává na sebe pozornost,provokuje."],
+    ["Je pravidelně připraven na vyučování a vždy vybaven potřebnými pomůckami. Svědomitě plní zadané domácí úkoly, jeho domácí příprava je pečlivá. V případě absence si samostatně zajistí doplnění zameškaného učiva a dopsání zápisů.",
+        "Je pravidelně připraven na vyučování a zpravidla vybaven potřebnými pomůckami. Domácí příprava je pečlivá, zadané úkoly až na výjimky plní svědomitě. V případě absence si většinou samostatně doplní zameškané učivo včetně zápisů.",
+    "Příprava na vyučování má rezervy, často nemá v pořádku některé pomůcky na vyučovací hodinu. Většina domácích úkolů je vypracována uspokojivě.
+*V případě absence si bez upozornění učitele nedoplní samostatně učivo.",
+        "Na vyučovací hodiny se většinou nepřipravuje, často chybí potřebné pomůcky. Projevuje se časté neplnění povinností a domácích úkolů.
+*V případě absence si bez opakovaného upozornění učitele nedoplní učivo.",
+        "Na vyučování se
+nepřipravuje, nenosí předepsané pomůcky. Má liknavý přístup k povinnostem a neplní zadané úkoly.
+*Ani po upozornění učitele není učivo doplněné.
+]
 ];
 
 // Matice přiřazující číselné hodnoty k textovým hodnotám
 let maticeHodnot = [
-    [5, 8, 3],
-    [7, 2, 9],
-    [4, 6, 1]
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 2, 3, 4, 5],
+    [0, 1, 2, 3, 4, 5],
 ];
 
 // Pole pro uchování hodnot buňek po kliknutí
