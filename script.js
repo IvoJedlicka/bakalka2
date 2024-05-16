5735af14fafb112daaf1f63fca9bf7923c0a266d
IvoJedlicka <jedlicka@bakalka.cz>
2024-05-16 07:40:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,14 +9,9 @@ let maticeText = [
      "Žák je v přístupu zcela pasivní. Nereaguje na domluvu. Negativně působí na třídu, vyrušuje a strhává na sebe pozornost,provokuje."],
     ["Je pravidelně připraven na vyučování a vždy vybaven potřebnými pomůckami. Svědomitě plní zadané domácí úkoly, jeho domácí příprava je pečlivá. V případě absence si samostatně zajistí doplnění zameškaného učiva a dopsání zápisů.",
         "Je pravidelně připraven na vyučování a zpravidla vybaven potřebnými pomůckami. Domácí příprava je pečlivá, zadané úkoly až na výjimky plní svědomitě. V případě absence si většinou samostatně doplní zameškané učivo včetně zápisů.",
-    "Příprava na vyučování má rezervy, často nemá v pořádku některé pomůcky na vyučovací hodinu. Většina domácích úkolů je vypracována uspokojivě.
-*V případě absence si bez upozornění učitele nedoplní samostatně učivo.",
-        "Na vyučovací hodiny se většinou nepřipravuje, často chybí potřebné pomůcky. Projevuje se časté neplnění povinností a domácích úkolů.
-*V případě absence si bez opakovaného upozornění učitele nedoplní učivo.",
-        "Na vyučování se
-nepřipravuje, nenosí předepsané pomůcky. Má liknavý přístup k povinnostem a neplní zadané úkoly.
-*Ani po upozornění učitele není učivo doplněné.
-]
+    "Příprava na vyučování má rezervy, často nemá v pořádku některé pomůcky na vyučovací hodinu. Většina domácích úkolů je vypracována uspokojivě.V případě absence si bez upozornění učitele nedoplní samostatně učivo.",
+        "Na vyučovací hodiny se většinou nepřipravuje, často chybí potřebné pomůcky. Projevuje se časté neplnění povinností a domácích úkolů. V případě absence si bez opakovaného upozornění učitele nedoplní učivo.",
+        "Na vyučování se nepřipravuje, nenosí předepsané pomůcky. Má liknavý přístup k povinnostem a neplní zadané úkoly. Ani po upozornění učitele není učivo doplněné.]
 ];
 
 // Matice přiřazující číselné hodnoty k textovým hodnotám
